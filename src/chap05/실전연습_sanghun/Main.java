2adb9ed19395944473a8afb1b5f7939d9569ebc9
sanghunDev <bsh20057@gmail.com>
2022-04-08 10:59:54 +0900

    perf: chap05 실전연습
    main method 추가

--- src/chap05/실전연습_sanghun/Main.java
+++ src/chap05/실전연습_sanghun/Main.java
@@ -4,20 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    Trader raoul = new Trader("Raoul", "Cambridge");
-    Trader mario = new Trader("Mario", "Milan");
-    Trader alan = new Trader("Alan", "Cambridge");
-    Trader brian = new Trader("Brian", "Cambridge");
-
-    List<Transaction> transactions = Arrays.asList(
-      new Transaction(brian,2011,300),
-      new Transaction(raoul,2012,1000),
-      new Transaction(raoul,2011,400),
-      new Transaction(mario,2012,710),
-      new Transaction(mario,2012,700),
-      new Transaction(alan,2012,950)
-    );
-
     /**
      *
      * 1. 2011년에 일어난 모든 트랜잭션을 찾아 값을 오름차순으로 정리하시오
@@ -38,5 +24,20 @@ public class Main {
      *
      */
 
+    public static void main(String[] args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
 
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian,2011,300),
+                new Transaction(raoul,2012,1000),
+                new Transaction(raoul,2011,400),
+                new Transaction(mario,2012,710),
+                new Transaction(mario,2012,700),
+                new Transaction(alan,2012,950)
+        );
+
+    }
 }
